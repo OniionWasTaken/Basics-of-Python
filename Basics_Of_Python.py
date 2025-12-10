ba5f88a378e1352623521fb42bd3990f7b5e760c
OniionWasTaken <dialingporchee@gmail.com>
2025-12-10 16:15:25 +0000

    Work given in class

---
+++ Basics_Of_Python.py
@@ -0,0 +1,41 @@
+#Question 1
+playlist = ["Birds of a Feather","Dance Monkey","Shape Of You","Levitating","Watermelon Sugar"]
+
+#Question 2
+top_artists = ("Bruno Mars", "Billie Eillish", "Drake", "Kendrick Lamar")
+
+#Question 3
+my_music = (playlist, top_artists)
+
+#Question 4
+print("Total items in my_music: " + str(len(my_music)))
+
+#Question 5
+print("Third song: " + playlist[2])
+
+#Question 6
+playlist[0] = "Perfect"
+
+#Question 7
+playlist.append("Blinding Lights")
+
+#Question 8
+print("Updated playlist: " + str(playlist))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
